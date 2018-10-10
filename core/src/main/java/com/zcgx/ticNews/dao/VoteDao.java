007d0f677e0ce2c721f4c2ef80399494b18ea7ca
dengyj3 <dengyj3@139.com>
2018-10-10 23:59:02 +0800

    commit: 修改查询的sql

--- core/src/main/java/com/zcgx/ticNews/dao/VoteDao.java
+++ core/src/main/java/com/zcgx/ticNews/dao/VoteDao.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
  *
  */
 public interface VoteDao extends JpaRepository<Vote, Long>, JpaSpecificationExecutor<Vote> {
-    @Query(value = "select * from jianbao.tbl_vote where article_id=?1 and uniondid=?2", nativeQuery = true)
+    @Query(value = "select * from jianbao.tbl_vote where article_id=?1 and unionid=?2", nativeQuery = true)
     Vote findByArticleIdAndUnionid(long articleId, String unionid);
     @Query(value = "select * from jianbao.tbl_tag where tag_name=?1", nativeQuery = true)
     Vote findByTagName(String tagName);
