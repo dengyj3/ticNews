cf0eda47cc393698ba7f9c344d29bc9d8965e740
dengyj3 <dengyj3@139.com>
2018-10-17 13:13:43 +0800

    Update TagArticleRelationService.java

--- core/src/main/java/com/zcgx/ticNews/service/TagArticleRelationService.java
+++ core/src/main/java/com/zcgx/ticNews/service/TagArticleRelationService.java
@@ -8,7 +8,7 @@ public interface TagArticleRelationService {
     TagArticleRelation addTagArticleRelation(TagArticleRelation tagArticleRelation);
     int deleteTagArticleRelation(long id);
     TagArticleRelation modifyTagArticleRelation(TagArticleRelation tagArticleRelation);
-    List<Long> queryTagArticleRelation(List<Long> tagId);
+    List<Integer> queryTagArticleRelation(List<Long> tagId);
 
     int deleteTagArticleRelationByArticleId(long articleId);
     int deleteTagArticleRelationByTagId(long tagId);
