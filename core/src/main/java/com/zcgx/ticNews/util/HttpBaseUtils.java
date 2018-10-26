fa34e43f2386340cc03ca3c6dea3a2332bc2628f
dengyj3 <dengyj3@139.com>
2018-10-26 15:38:44 +0800

    Update HttpBaseUtils.java

--- core/src/main/java/com/zcgx/ticNews/util/HttpBaseUtils.java
+++ core/src/main/java/com/zcgx/ticNews/util/HttpBaseUtils.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -27,5 +29,29 @@ public class HttpBaseUtils {
         HttpEntity<?> entity = new HttpEntity<>(headers);
         ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         return responseEntity.getBody();
+    }
+
+    public static String postRequest(RestTemplate restTemplate, String url, Map<String, String> headerMap, Map<String, String> paramMap){
+        MultiValueMap<String, Object> postParameters = new LinkedMultiValueMap<>();
+        HttpHeaders headers = new HttpHeaders();
+        if (null != headerMap){
+            headerMap.forEach((k, v) -> {
+                headers.add(k, v);
+            });
+        }
+
+        if (null != paramMap){
+            paramMap.forEach((k, v) -> {
+                postParameters.add(k, v);
+            });
+        }
+//        postParameters.add("userCode", "291974");
+
+//        headers.add("Content-Type", "application/x-www-form-urlencoded");
+        HttpEntity<MultiValueMap<String, Object>> r = new HttpEntity<>(postParameters, headers);
+
+        String data= restTemplate.postForObject(url, r, String.class);
+//        System.out.println(data);
+        return data;
     }
 }
